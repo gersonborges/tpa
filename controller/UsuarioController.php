b2bee65b6feeda48ecdc33442fd6e7c97a2cbb1f
Gerson Borges <passupreto@gmail.com>
2020-04-14 07:37:48 -0300

    Update UsuarioController.php

--- controller/UsuarioController.php
+++ controller/UsuarioController.php
@@ -29,6 +29,21 @@ class UsuarioController
 
         require_once "view/Usuario_create.php";
     }
+    
+    public function update()
+    {
+        $obj = new Usuario();
+        $obj->setId($_GET['id']);
+
+        if (isset($_POST["email"]) && isset($_POST["nome"]) ) {
+            $obj->setEmail($_POST["email"]);
+            $obj->setNome($_POST["nome"]);
+            $obj->update();
+        }
+
+        $usuario = $obj->read();
+        include "view/Usuario_update.php";
+    }
 
 }
 
