d298ba6dc715f9a70fd65c2cb089423a3256d08c
Gerson Borges <passupreto@gmail.com>
2020-04-18 13:45:41 -0300

    Update Usuario_all.php

--- view/Usuario_all.php
+++ view/Usuario_all.php
@@ -21,8 +21,44 @@
                 <td> <?= $usuario->id ?> </td>
                 <td> <?= $usuario->email ?> </td>
                 <td> <?= $usuario->nome ?> </td>
-                <td><a class="btn btn-primary" href="./?classe=Usuario&acao=update&id=<?= $usuario->id ?>">Editar</a> </td>
+                <td>
+                    <a class="btn btn-primary" href="./?classe=Usuario&acao=update&id=<?= $usuario->id ?>">Editar</a> 
+                    <button
+                                type='button'
+                                class='btn btn-danger'
+                                data-toggle='modal'
+                                data-target='#exclui-modal-<?=$usuario->id?>'
+                                data-id='<?=$usuario->id?>'>
+                            Excluir
+                    </button>                
+                </td>
             </tr>
+        
+        
+            <div class="modal fade" id="exclui-modal-<?=$usuario->id?>">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+
+                            <!-- Cabeçalho -->
+                            <div class="modal-header">
+                                <h4 class="modal-title">Excluir</h4>
+                                <button type="button" class="close" data-dismiss="modal">×</button>
+                            </div>
+
+                            <!-- Corpo -->
+                            <div class="modal-body">
+                                Confirma exclusão?
+                            </div>
+
+                            <!-- Rodapé -->
+                            <div class="modal-footer">
+                                <a class="btn btn-primary" href="./?classe=Usuario&acao=delete&id=<?= $usuario->id ?>">Sim</a>
+                                <button type="button" class="btn btn-danger" data-dismiss="modal">Não</button>
+                            </div>
+
+                        </div>
+                    </div>
+            </div>
 
         <?php } ?>
 
