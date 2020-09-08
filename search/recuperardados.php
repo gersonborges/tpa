552ac1db787ee4230b5049059abe81a38cfe91ad
Gerson Borges <passupreto@gmail.com>
2020-09-08 08:34:22 -0300

    Update recuperardados.php

--- search/recuperardados.php
+++ search/recuperardados.php
@@ -2,19 +2,7 @@
 require_once('config.php');
 $con = new PDO(SERVIDOR, USUARIO, SENHA);
 $sql = $con->prepare("
-SELECT
-  p.*,
-  e.cep,
-  e.logradouro,
-  e.numero,
-  e.complemento,
-  e.bairro,
-  e.cidade,
-  e.uf
-FROM prestador p
-LEFT JOIN endereco e ON p.id=e.prestador_id
-ORDER BY p.nome");
+SELECT nome FROM prestador ORDER BY nome");
 $sql->execute();
 $rows = $sql->fetchAll(PDO::FETCH_CLASS);
-
 echo json_encode($rows);
