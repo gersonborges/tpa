d1131709dfabd31fa12a61e96f04c92ec3696962
Gerson Borges <passupreto@gmail.com>
2020-10-06 11:23:05 -0300

    Update atividade.sql

--- graficos/atividade.sql
+++ graficos/atividade.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS `atividade` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `trimestre` int(11) NOT NULL,
   `descricao` varchar(45) NOT NULL,
   `aluno` varchar(45) NOT NULL,
-  `nota` double NOT NULL,
+  `nota` decimal(5,1) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
