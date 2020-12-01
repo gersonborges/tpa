e0c0f3f31ad06d0838b8c55bae78e30ec666a2cc
Gerson Borges <passupreto@gmail.com>
2020-12-01 09:37:48 -0300

    Update produtos.php

--- jequiti/ecommerce/produtos.php
+++ jequiti/ecommerce/produtos.php
@@ -1,57 +1,70 @@
 <?php
-$produtos =[1,1,1,1,1];
+session_start();
+header("Content-Type: text/html; charset=utf8");
+date_default_timezone_set('America/Argentina/Buenos_Aires');
+define("SERVIDOR","mysql:host=localhost;dbname=jequiti3a2;charset=utf8");
+define("USUARIO","root");
+define("SENHA","");
+$con = new PDO(SERVIDOR, USUARIO, SENHA);
+$sql = $con->prepare("SELECT * FROM ver_produto");
+$sql->execute();
+$produtos = $sql->fetchAll(PDO::FETCH_CLASS);
 
-foreach($produtos as $produto){
-?>
+foreach ($produtos as $produto) {
+
+    $sql = $con->prepare("SELECT * FROM usuario_produto WHERE usuario_id=? AND produto_id=? LIMIT 1");
+    $sql->execute([ $_SESSION['usuario']->id, $produto->id ]);
+    $like = $sql->fetchObject();
+    $cor = $like ? 'red' : 'gray';
+
+    ?>
     <!-- produto -->
     <div class="col-md-3" style="float: left;">
-    <div class="card" style="float: left; margin-right: 15px">
-        <div class="card-header">
-            <h4 class="card-title"><strong><a href="">Fornecedor</a></strong></h4>
-            <div class="card-tools">
-                <span class="badge badge-primary">Categoria</span>
+        <div class="card" style="float: left; margin-right: 15px">
+            <div class="card-header">
+                <h4 class="card-title"><strong><a href="">Fornecedor</a></strong></h4>
+                <div class="card-tools">
+                    <span class="badge badge-primary">Categoria</span>
+                </div>
             </div>
-        </div>
-        <div class="view view-cascade">
-            <img class="card-img-top"
-                 src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Products/shoes%20(3).jpg"
-                 alt="">
-            <a>
-                <div class="mask rgba-white-slight"></div>
-            </a>
-        </div>
+            <div class="view view-cascade">
+                <?= $produto->figura ?>
+                <a>
+                    <div class="mask rgba-white-slight"></div>
+                </a>
+            </div>
 
-        <div class="card-body">
-            <h5 class="text-center">Produto</h5>
-            <h6 class="text-center">
-                <ul class="rating m-auto">
-                    <li><i class="fas fa-star"></i></li>
-                    <li><i class="fas fa-star"></i></li>
-                    <li><i class="fas fa-star"></i></li>
-                    <li><i class="fas fa-star"></i></li>
-                    <li><i class="far fa-star"></i></li>
-                </ul>
-            </h6>
-            <p class="card-text">Temporibus autem quibusdam et aut officiis debitis aut rerum
-                necessitatibus saepe
-                eveniet ut et voluptates.
-            </p>
-        </div>
-        <div class="card-footer">
-            <span class="float-left">49$</span>
-            <span class="float-right">
+            <div class="card-body">
+                <h5 class="text-center"><?= $produto->produto ?></h5>
+                <h6 class="text-center">
+                    <ul class="rating m-auto">
+                        <li><i class="fas fa-star"></i></li>
+                        <li><i class="fas fa-star"></i></li>
+                        <li><i class="fas fa-star"></i></li>
+                        <li><i class="fas fa-star"></i></li>
+                        <li><i class="far fa-star"></i></li>
+                    </ul>
+                </h6>
+                <p class="card-text">Temporibus autem quibusdam et aut officiis debitis aut rerum
+                    necessitatibus saepe
+                    eveniet ut et voluptates.
+                </p>
+            </div>
+            <div class="card-footer">
+                <span class="float-left" style="font-size: xx-large">R$ <?= number_format($produto->preco_venda,2,',','') ?></span>
+                <span class="float-right">
                                         <a data-toggle="tooltip" data-placement="top" title="Add to Cart"><i
                                                     class="fas fa-shopping-cart mr-3"></i></a>
                                         <a data-toggle="tooltip" data-placement="top" title="Share"><i
                                                     class="fas fa-share-alt mr-3"></i></a>
-                                        <a class="active" data-toggle="tooltip" data-placement="top"
-                                           title="Added to Wishlist"><i class="fas fa-heart"></i></a>
+                                        <a href="inserirLike.php?produto_id=<?=$produto->id?>" class="active" data-toggle="tooltip" data-placement="top"
+                                           title="Added to Wishlist"><i class="fas fa-heart" style="color:<?=$cor?>" ></i></a>
                                     </span>
+            </div>
         </div>
     </div>
-    </div>
     <!-- produto -->
-<?php
+    <?php
 }
 ?>
 
