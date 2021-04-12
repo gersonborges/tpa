233cacc2fd2407552911fef5fc1e0891483debaa
Gerson Borges <passupreto@gmail.com>
2021-04-12 15:10:27 -0300

    Update index.php

--- mvc/index.php
+++ mvc/index.php
@@ -7,11 +7,3 @@ $app = new UsuarioController();
 $app->all();
 
 
-/*
-require_once "inc/config.php";
-require_once "controller/ClienteController.php";
-require_once "model/Cliente.php";
-
-$app = new ClienteController();
-$app->teste();
-*/
