c4ad832bb5226431c8ca93998a093ba29a72794e
Gerson Borges <passupreto@gmail.com>
2021-05-24 15:01:26 -0300

    Create usuario_create.php

---
+++ crud/view/usuario_create.php
@@ -0,0 +1,18 @@
+<html>
+
+<head></head>
+
+<body>
+
+    <form action="" method="post">
+
+        Email: <input type="email" name="email"/><br>
+        Nome: <input type="text" name="nome"?><br>
+
+        <button type="submit">Salvar</button>
+    
+    </form>
+
+</body>
+
+</html>
